6f5497b92fd70f09b3ad08ab0829fe421068d2ea
Cole  Shanks <coleshanks@Coles-MacBook-Pro.local>
2023-06-12 02:43:49 -0700

    fixed waiting for instance

--- run_setup.sh
+++ run_setup.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
+#initalize terraform any apply
 terraform init
 terraform apply
+
+#save ip addr
 IP="$(terraform output -raw instance_ip_addr)"
 echo "$IP" > ip_addr.txt
+
+#save DNS to pass as arg
 DNS="$(terraform output -raw instance_public_dns)"
+
+#make readable
 chmod 400 ubuntu-key-pair.pem
-#wait for instance to be ready
-#aws ec2 wait instance-status-ok --instance-ids "$(terraform output -raw instance_id)"
+
+#get instance id to pass as arg
 INSTANCE_ID="$(terraform output -raw instance_id)"
-bash wait_for_instance.sh $INSTANCE_ID
+
+#wait for instance to be ready for ssh connection
+bash wait_for_instance.sh $DNS $INSTANCE_ID 5
+
+#setup minecraft on server, enable service
 bash setup_minecraft.sh $DNS
-bash wait_for_instance.sh $INSTANCE_ID
+
+#wait again
+bash wait_for_instance.sh $DNS $INSTANCE_ID 20
 echo "########## setup complete connect to $IP ##########"
--- wait_for_instance.sh
+++ wait_for_instance.sh
@@ -1,4 +1,13 @@
-while STATE=$(aws ec2 describe-instances --instance-ids $1 --output text --query 'Reservations[*].Instances[*].State.Name'); test "$STATE" != "running"; do
+while STATE=$(aws ec2 describe-instances --instance-ids $2 --output text --query 'Reservations[*].Instances[*].State.Name'); test "$STATE" != "running"; do
     sleep 1;
 done;
 
+READY=false
+until $READY; do
+    echo "- Waiting for connection"
+    sleep $3
+    set +e
+    OUT=$(ssh -o ConnectTimeout=1 -o StrictHostKeyChecking=no -o BatchMode=yes ubuntu@$1 2>&1 | grep 'Permission denied' )
+    [[ $? = 0 ]] && READY=true
+    set -e
+done
