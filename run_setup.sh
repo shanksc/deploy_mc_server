1e23e9422d1d9d0c7263b774d93fd282a9c10e81
Cole  Shanks <coleshanks@Coles-MacBook-Pro.local>
2023-06-11 23:45:43 -0700

    fixed /n

--- run_setup.sh
+++ run_setup.sh
@@ -2,7 +2,7 @@
 terraform init
 terraform apply
 IP="$(terraform output -raw instance_ip_addr)"
-echo "$IP\n" > ip_addr.txt
+echo "$IP" > ip_addr.txt
 DNS="$(terraform output -raw instance_public_dns)"
 chmod 400 linux-key-pair.pem
 bash setup_minecraft.sh $DNS
